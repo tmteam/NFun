24ace67c85e8f5ac943331a9bd94e94e52c855ba
tmteam <tmteam@yandex.ru>
2019-03-23 15:06:26 +0300

    Pipe forward tests (not passed)

---
+++ src/Funny.Tests/PipeForwardTest.cs
@@ -0,0 +1,64 @@
+using System;
+using Funny.Types;
+using NUnit.Framework;
+
+namespace Funny.Tests
+{
+    [TestFixture]
+    public class PipeForwardTest
+    {
+        [TestCase( @"rr(x:real):bool = x>10
+                     y = [11.0,20.0,1.0,2.0]|> filter(rr)",new[]{11.0,20.0})]
+        [TestCase( @"ii(x:int):bool = x>10
+                     y = [11,20,1,2] |> filter(ii)",new[]{11,20})]
+        [TestCase( @"ii(x:int):int = x*x
+                     y = [1,2,3]|> map(ii)",new[]{1,4,9})]
+        
+        [TestCase( @"f(x:int):bool = x>10
+                     m(x:int):int  = x*x
+                     y = [11,20,1,2] 
+                            |> filter(f)
+                            |> map(m)", new[]{121,400})]
+        [TestCase( @"f(x:int):bool = x>10
+                     m(x:int):int  = x*x
+                     y = [11,20,1,2] 
+                            |> filter(f)
+                            |> map(m)
+                            |> max", 400)]
+        [TestCase( @"y = [1,2,3]|>max",3)]
+        [TestCase( @"y = [1,2,3]|>max()",3)]
+
+        [TestCase( @"f(x:int):int = x*x y = 4|>f",16)]
+        [TestCase( @"f(x:int):int = x+1 y = 4|>f|>f|>f",7)]
+        [TestCase( @"f(x:int):int = x*x y = -4|>f",16)]
+
+        [TestCase( @"f(x:int):int = x*x y = 4|>f() == f(4)",true)]
+        public void ConstantTest(string expr, object expected)
+        {
+            var runtime = Interpreter.BuildOrThrow(expr);
+            runtime.Calculate()
+                .AssertReturns(Var.New("y", expected));
+        }
+        
+        [TestCase( @"f(x:int):int = x*x y = 4|>f)")]
+        [TestCase( @"f(x:int):int = x*x y = 4|>f( == f(4)")]
+        [TestCase( @"f(x:int):int = x*x y = 4|>f(,) == f(4)")]
+        [TestCase( @"f(x:int):int = x*x y = 4|>f(1) == f(4)")]
+        [TestCase( @"f(x:int):int = x*x y = 4|>f(1,2) == f(4)")]
+        [TestCase( @"y = f|>4( == f(4)")]
+        [TestCase( @"y = 4|>f")]
+        [TestCase( @"y = f|>4")]
+        [TestCase( @"y = f|")]
+        public void ObviousFails(string expr)
+        {
+            try
+            {
+                Interpreter.BuildOrThrow(expr);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOf<ParseException>(e);
+            }
+        }
+    }
+}
