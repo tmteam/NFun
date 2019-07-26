d8fc78969cc09407da273856b039b3a8ad543dd2
Egor Ryzhukhin <yryzhukhin@plazius.ru>
2019-07-26 20:26:15 +0300

    Revert "Change and revert commit after merge"
    This reverts commit f040552c12fed4ddc24fb795d1c638103c3bd09c.

--- src/NFun.Fuspec/Parser/Model/Param.cs
+++ src/NFun.Fuspec/Parser/Model/Param.cs
@@ -4,13 +4,11 @@ namespace Nfun.Fuspec.Parser.Model
     {
         public string Value { get; }
         public string VarType { get; }
-        
 
         public Param(string value, string varType)
         {
             Value = value;
             VarType = varType;
-            
         }
     }
 }
