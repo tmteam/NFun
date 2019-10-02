e254e97f7d840524afcb8d9de46715b623cb490f
NataSh <N.Shepina@bk.ru>
2019-10-02 18:10:24 +0300

    Refactoring SetCheckPair

--- src/NFun.Fuspec/Parser/Model/SetCheckPair.cs
+++ src/NFun.Fuspec/Parser/Model/SetCheckPair.cs
@@ -6,33 +6,21 @@ namespace Nfun.Fuspec.Parser.Model
 {
     public class SetCheckPair
     {
-        private List<VarVal> _set;
-        private List<VarVal> _check;
+        readonly List<VarVal> _set;
+        readonly List<VarVal> _check;
 
-        public VarVal[] Set
-        {
-            get { return _set.ToArray(); }
-        }
+        public VarVal[] Set => _set.ToArray();
 
-        public VarVal[] Check
-        {
-            get{return _check.ToArray();}
-        }
+        public VarVal[] Check => _check.ToArray();
 
         public SetCheckPair()
         {
             _set = new List<VarVal>();
-            _check=new List<VarVal>();
+            _check = new List<VarVal>();
         }
 
-        internal void AddSet(VarVal[] set)
-        {
-            _set.AddRange(set); 
-        }
+        internal void AddSet(IEnumerable<VarVal> set) => _set.AddRange(set);
 
-        internal void AddGet(VarVal[] get)
-        {
-            _check.AddRange(get);
-        }
+        internal void AddGet(IEnumerable<VarVal> get) => _check.AddRange(get);
     }
 }
