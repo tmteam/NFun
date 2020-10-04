05a755150bb1f9a86c2dc4f382ec63b6d6e0ecea
tmteam <tmteam@yandex.ru>
2020-10-04 15:58:07 +0300

    Fix input funtype determination

--- src/NFun/BuiltInFunctions/TextFunctions.cs
+++ src/NFun/BuiltInFunctions/TextFunctions.cs
@@ -65,7 +65,7 @@ namespace NFun.BuiltInFunctions
     {
         public TrimFunction() : base("trim",VarType.Text,VarType.Text){}
 
-        public override object Calc(object a) => ((IFunArray) a).ToText().TrimStart().AsFunText();
+        public override object Calc(object a) => ((IFunArray) a).ToText().Trim().AsFunText();
     }
     
     public class TrimStartFunction : FunctionWithSingleArg
@@ -90,12 +90,15 @@ namespace NFun.BuiltInFunctions
             VarType.Text){
         }
 
-        public override object Calc(object a, object b) =>
-            new ImmutableFunArray(
-                TypeHelper.GetFunText(a)
-                    .Split( new[] {TypeHelper.GetFunText(b)}, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(s=>new TextFunArray(s))
+        public override object Calc(object a, object b)
+        {
+            var inputString = TypeHelper.GetFunText(a);
+            var delimeter   = TypeHelper.GetFunText(b);
+            return new ImmutableFunArray(
+                    inputString.Split(new[] {delimeter}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => new TextFunArray(s))
                     .ToArray());
+        }
     }
     
     
---
+++ src/NFun/Types/FunTypesConverter.cs
@@ -0,0 +1,85 @@
+using System;
+using NFun.Runtime.Arrays;
+
+namespace NFun.Types
+{
+    public abstract class FunTypesConverter
+    {
+        public static bool TryGetSpecificConverter(Type clrType, out FunTypesConverter converter)
+        {
+            converter = default;
+            if (clrType == typeof(string))
+            {
+                converter = new StringFunTypesConverter();
+                return true;
+            }
+
+            if (clrType.IsArray)
+            {
+                var elementType = clrType.GetElementType();
+                if(elementType == typeof(char))
+                    converter =  new StringFunTypesConverter();
+                if(TryGetSpecificConverter(elementType, out var elementConverter))
+                    converter = new ArrayFunTypesComplexConverter(elementConverter);
+                else
+                    converter = new ArrayFunTypesConverter(VarVal.ToPrimitiveFunType(elementType));
+                return true;
+            }
+            return false;
+        }
+        protected FunTypesConverter(VarType funType)
+        {
+            FunType = funType;
+        }
+
+        public VarType FunType { get; }
+        public abstract object ToFunObject(object clrObject);
+    }
+
+    public class ArrayFunTypesComplexConverter : FunTypesConverter
+    {
+        private readonly FunTypesConverter _elementConverter;
+
+        public ArrayFunTypesComplexConverter(FunTypesConverter elementConverter) 
+            : base(VarType.ArrayOf(elementConverter.FunType))
+        {
+            _elementConverter = elementConverter;
+        }
+
+        public override object ToFunObject(object clrObject)
+        {
+            var array = clrObject as Array;
+            object[] converted = new object[array.Length]; 
+            for (int i = 0; i < array.Length; i++)
+            {
+                converted[i] = _elementConverter.ToFunObject(array.GetValue(i));
+            }
+            return new ImmutableFunArray(converted);
+        }
+    }
+    
+    public class ArrayFunTypesConverter : FunTypesConverter
+    {
+        public ArrayFunTypesConverter(VarType elementType) 
+            : base(VarType.ArrayOf(elementType))
+        {
+        }
+
+        public override object ToFunObject(object clrObject) 
+            => new ImmutableFunArray(((Array) clrObject));
+    }
+    public class CharArrayFunTypesConverter: FunTypesConverter
+    {
+        public CharArrayFunTypesConverter() : base(VarType.Text)
+        {
+        }
+        public override object ToFunObject(object clrObject) => new TextFunArray(new string((char[]) clrObject));
+    }
+    public class StringFunTypesConverter: FunTypesConverter
+    {
+        public StringFunTypesConverter() : base(VarType.Text)
+        {
+        }
+        public override object ToFunObject(object clrObject) => new TextFunArray(clrObject.ToString());
+    }
+}
