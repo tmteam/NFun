db82a116cd901ff5fa3e03cd6c77b002d03b4de2
tmteam <tmteam@yandex.ru>
2020-10-10 14:13:49 +0300

    Fix debug namespaces

--- src/NFun/Tic/TicNode.cs
+++ src/NFun/Tic/TicNode.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using NFun.Tic.SolvingStates;
