a06835a22ad1cbc5d7bd99e75f40b24b0a4e8c7c
tmteam <tmteam@yandex.ru>
2020-10-16 01:34:14 +0300

    Cover integer bit operators

--- src/NFun.Tests/Operators/IntegerBitOperatorsTest.cs
+++ src/NFun.Tests/Operators/IntegerBitOperatorsTest.cs
@@ -7,32 +7,184 @@ namespace Funny.Tests.Operators
 {
     public class IntegerBitOperatorsTest
     {
-        [TestCase("y = 1 & 1",1)]
-        [TestCase("y = 1 & 2",0)]
-        [TestCase("y = 1 & 3",1)]
+        [TestCase("y:int64 = 1 & 1",(Int64)1)]
+        [TestCase("y:int64 = 1 & 2",(Int64)0)]
+        [TestCase("y:int64 = 2 & 2",(Int64)2)]
+        [TestCase("y:int64 = 1 & 3",(Int64)1)]
+        [TestCase("y:int64 = 1 & 3",(Int64)1)]
+
+        [TestCase("y:int32 = 1 & 1",(Int32)1)]
+        [TestCase("y:int32 = 1 & 2",(Int32)0)]
+        [TestCase("y:int32 = 2 & 2",(Int32)2)]
+        [TestCase("y:int32 = 1 & 3",(Int32)1)]
+        [TestCase("y:int16 = 1 & 1",(Int16)1)]
+        [TestCase("y:int16 = 1 & 2",(Int16)0)]
+        [TestCase("y:int16 = 2 & 2",(Int16)2)]
+        [TestCase("y:int16 = 1 & 3",(Int16)1)]
+        
+        [TestCase("y:uint64 = 1 & 1",(UInt64)1)]
+        [TestCase("y:uint64 = 1 & 2",(UInt64)0)]
+        [TestCase("y:uint64 = 2 & 2",(UInt64)2)]
+        [TestCase("y:uint64 = 1 & 3",(UInt64)1)]
+        [TestCase("y:uint32 = 1 & 1",(UInt32)1)]
+        [TestCase("y:uint32 = 1 & 2",(UInt32)0)]
+        [TestCase("y:uint32 = 2 & 2",(UInt32)2)]
+        [TestCase("y:uint32 = 1 & 3",(UInt32)1)]
+        [TestCase("y:uint16 = 1 & 1",(UInt16)1)]
+        [TestCase("y:uint16 = 1 & 2",(UInt16)0)]
+        [TestCase("y:uint16 = 2 & 2",(UInt16)2)]
+        [TestCase("y:uint16 = 1 & 3",(UInt16)1)]
+        
+        [TestCase("y:uint8 = 1 & 1",(byte)1)]
+        [TestCase("y:uint8 = 1 & 2",(byte)0)]
+        [TestCase("y:uint8 = 2 & 2",(byte)2)]
+        [TestCase("y:uint8 = 1 & 3",(byte)1)]
+
+        
         [TestCase("y = 0xFFFFFFFF & 0x0", (long)0)]
         [TestCase("y = 0xFFFFFFFF & 0xFFFFFFFF", (long)0xFFFFFFFF)]
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF & 0xFFFFFFFF_FFFFFFFF",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF & 0",(UInt64)0)]
+        [TestCase("y:uint64 = 0 & 0xFFFFFFFF_FFFFFFFF",(UInt64)0)]
+        [TestCase("y:uint32 = 0xFFFFFFFF & 0xFFFFFFFF",(UInt32)0xFFFFFFFF)]
+        [TestCase("y:uint32 = 0xFFFFFFFF & 0",(UInt32)0)]
+        [TestCase("y:uint32 = 0 & 0xFFFFFFFF",(UInt32)0)]
+        [TestCase("y:uint16 = 0xFFFF & 0xFFFF",(UInt16)0xFFFF)]
+        [TestCase("y:uint16 = 0xFFFF & 0",(UInt16)0)]
+        [TestCase("y:uint16 = 0 & 0xFFFF",(UInt16)0)]
+        [TestCase("y:uint8 = 0xFF & 0xFF",(byte)0xFF)]
+        [TestCase("y:uint8 = 0xFF & 0",(byte)0)]
+        [TestCase("y:uint8 = 0 & 0xFF",(byte)0)]
         public void ConstantBitAnd(string expression, object expected) 
             => TestTools.AssertConstantCalc("y",expression, expected);
         
         [TestCase("y = 0 | 2",2)]
         [TestCase("y = 1 | 2",3)]
         [TestCase("y = 1 | 4",5)]
+        
+        [TestCase("y:int64 = 1 | 1",(Int64)1)]
+        [TestCase("y:int64 = 1 | 2",(Int64)3)]
+        [TestCase("y:int64 = 2 | 2",(Int64)2)]
+        [TestCase("y:int64 = 1 | 3",(Int64)3)]
+        [TestCase("y:int64 = 1 | 3",(Int64)3)]
+
+        [TestCase("y:int32 = 1 | 1",(Int32)1)]
+        [TestCase("y:int32 = 1 | 2",(Int32)3)]
+        [TestCase("y:int32 = 2 | 2",(Int32)2)]
+        [TestCase("y:int32 = 1 | 3",(Int32)3)]
+        [TestCase("y:int16 = 1 | 1",(Int16)1)]
+        [TestCase("y:int16 = 1 | 2",(Int16)3)]
+        [TestCase("y:int16 = 2 | 2",(Int16)2)]
+        [TestCase("y:int16 = 1 | 3",(Int16)3)]
+        
+        [TestCase("y:uint64 = 1 | 1",(UInt64)1)]
+        [TestCase("y:uint64 = 1 | 2",(UInt64)3)]
+        [TestCase("y:uint64 = 2 | 2",(UInt64)2)]
+        [TestCase("y:uint64 = 1 | 3",(UInt64)3)]
+        [TestCase("y:uint32 = 1 | 1",(UInt32)1)]
+        [TestCase("y:uint32 = 1 | 2",(UInt32)3)]
+        [TestCase("y:uint32 = 2 | 2",(UInt32)2)]
+        [TestCase("y:uint32 = 1 | 3",(UInt32)3)]
+        [TestCase("y:uint16 = 1 | 1",(UInt16)1)]
+        [TestCase("y:uint16 = 1 | 2",(UInt16)3)]
+        [TestCase("y:uint16 = 2 | 2",(UInt16)2)]
+        [TestCase("y:uint16 = 1 | 3",(UInt16)3)]
+        
+        [TestCase("y:uint8 = 1 | 1",(byte)1)]
+        [TestCase("y:uint8 = 1 | 2",(byte)3)]
+        [TestCase("y:uint8 = 2 | 2",(byte)2)]
+        [TestCase("y:uint8 = 1 | 3",(byte)3)]
+        
         [TestCase("y = 0xFFFFFFFF | 0x0",(long)0xFFFFFFFF)]
         [TestCase("y = 0xFFFFFFFF | 0xFFFFFFFF",(long)0xFFFFFFFF)]
+        
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF | 0xFFFFFFFF_FFFFFFFF",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF | 0",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+        [TestCase("y:uint64 = 0 | 0xFFFFFFFF_FFFFFFFF",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+        [TestCase("y:uint32 = 0xFFFFFFFF | 0xFFFFFFFF",(UInt32)0xFFFFFFFF)]
+        [TestCase("y:uint32 = 0xFFFFFFFF | 0",(UInt32)0xFFFFFFFF)]
+        [TestCase("y:uint32 = 0 | 0xFFFFFFFF",(UInt32)0xFFFFFFFF)]
+        [TestCase("y:uint16 = 0xFFFF | 0xFFFF",(UInt16)0xFFFF)]
+        [TestCase("y:uint16 = 0xFFFF | 0",(UInt16)0xFFFF)]
+        [TestCase("y:uint16 = 0 | 0xFFFF",(UInt16)0xFFFF)]
+        [TestCase("y:uint8 = 0xFF | 0xFF",(byte)0xFF)]
+        [TestCase("y:uint8 = 0xFF | 0",(byte)0xFF)]
+        [TestCase("y:uint8 = 0 | 0xFF",(byte)0xFF)]
         public void ConstantBitOr(string expression, object expected) 
             => TestTools.AssertConstantCalc("y",expression, expected);
+
+        [TestCase("y:int64 = 0 ^ 1",(Int64)1)]
+        [TestCase("y:int64 = 1 ^ 0",(Int64)1)]
+        [TestCase("y:int64 = 1 ^ 1",(Int64)0)]
+        [TestCase("y:int64 = 0 ^ 0",(Int64)0)]
+        [TestCase("y:int32 = 0 ^ 1",(Int32)1)]
+        [TestCase("y:int32 = 1 ^ 0",(Int32)1)]
+        [TestCase("y:int32 = 1 ^ 1",(Int32)0)]
+        [TestCase("y:int32 = 0 ^ 0",(Int32)0)]
+        [TestCase("y:int16 = 0 ^ 1",(Int16)1)]
+        [TestCase("y:int16 = 1 ^ 0",(Int16)1)]
+        [TestCase("y:int16 = 1 ^ 1",(Int16)0)]
+        [TestCase("y:int16 = 0 ^ 0",(Int16)0)]
         
-        [TestCase("y = 1 ^ 0",1)]
-        [TestCase("y = 1 ^ 1",0)]
-        [TestCase("y = 1 ^ 1",0)]
+      
+        [TestCase("y:uint64 = 0 ^ 1",(UInt64)1)]
+        [TestCase("y:uint64 = 1 ^ 0",(UInt64)1)]
+        [TestCase("y:uint64 = 1 ^ 1",(UInt64)0)]
+        [TestCase("y:uint64 = 0 ^ 0",(UInt64)0)]
+        [TestCase("y:uint32 = 0 ^ 1",(UInt32)1)]
+        [TestCase("y:uint32 = 1 ^ 0",(UInt32)1)]
+        [TestCase("y:uint32 = 1 ^ 1",(UInt32)0)]
+        [TestCase("y:uint32 = 0 ^ 0",(UInt32)0)]
+        [TestCase("y:uint16 = 0 ^ 1",(UInt16)1)]
+        [TestCase("y:uint16 = 1 ^ 0",(UInt16)1)]
+        [TestCase("y:uint16 = 1 ^ 1",(UInt16)0)]
+        [TestCase("y:uint16 = 0 ^ 0",(UInt16)0)]
+        [TestCase("y:uint8 = 0 ^ 1",(byte)1)]
+        [TestCase("y:uint8 = 1 ^ 0",(byte)1)]
+        [TestCase("y:uint8 = 1 ^ 1",(byte)0)]
+        [TestCase("y:uint8 = 0 ^ 0",(byte)0)]
+        
         [TestCase("y = 0xFFFFFFFF ^ 0x0", (long)0xFFFFFFFF)]
         [TestCase("y = 0xFFFFFFFF ^ 0xFFFFFFFF",(long)0)]
+        
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF ^ 0xFFFFFFFF_FFFFFFFF",(UInt64)0)]
+        [TestCase("y:uint64 = 0xFFFFFFFF_FFFFFFFF ^ 0",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+        [TestCase("y:uint64 = 0 ^ 0xFFFFFFFF_FFFFFFFF",(UInt64)0xFFFFFFFF_FFFFFFFF)]
+
+        [TestCase("y:uint32 = 0xFFFFFFFF ^ 0xFFFFFFFF",(UInt32)0)]
+        [TestCase("y:uint32 = 0xFFFFFFFF ^ 0",(UInt32)0xFFFFFFFF)]
+        [TestCase("y:uint32 = 0 ^ 0xFFFFFFFF",(UInt32)0xFFFFFFFF)]
+        
+        [TestCase("y:uint16 = 0xFFFF ^ 0xFFFF",(UInt16)0)]
+        [TestCase("y:uint16 = 0xFFFF ^ 0",(UInt16)0xFFFF)]
+        [TestCase("y:uint16 = 0 ^ 0xFFFF",(UInt16)0xFFFF)]
+        
+        [TestCase("y:uint8 = 0xFF ^ 0xFF",(byte)0)]
+        [TestCase("y:uint8 = 0xFF ^ 0",(byte)0xFF)]
+        [TestCase("y:uint8 = 0 ^ 0xFF",(byte)0xFF)]
         public void ConstantBitXor(string expression, object expected) 
             => TestTools.AssertConstantCalc("y",expression, expected);
         
         [TestCase("y = 1 << 3",8)]
         [TestCase("y = 8 >> 3",1)]
+        [TestCase("y:int64 = 1 << 3",(Int64)8)]
+        [TestCase("y:int64 = 8 >> 3",(Int64)1)]
+        [TestCase("y:int32 = 1 << 3",(Int32)8)]
+        [TestCase("y:int32 = 8 >> 3",(Int32)1)]
+        
+        [TestCase("y:uint64 = 1 << 3",(UInt64)8)]
+        [TestCase("y:uint64 = 8 >> 3",(UInt64)1)]
+        [TestCase("y:uint32 = 1 << 3",(UInt32)8)]
+        [TestCase("y:uint32 = 8 >> 3",(UInt32)1)]
+        
+        // Todo ui16 i16, u8 does not support shift.
+        // Should they?
+        //[TestCase("y:int16 = 1 << 3",(Int16)8)]
+        //[TestCase("y:int16 = 8 >> 3",(Int16)1)]
+        //[TestCase("y:uint16 = 1 << 3",(UInt16)8)]
+        //[TestCase("y:uint16 = 8 >> 3",(UInt16)1)]
+        //[TestCase("y:byte = 1 << 3",(byte)8)]
+        //[TestCase("y:byte = 8 >> 3",(byte)1)]
         public void ConstantBitShift(string expression, object expected) 
             => TestTools.AssertConstantCalc("y",expression, expected);
         
@@ -65,7 +217,27 @@ namespace Funny.Tests.Operators
         [TestCase("y = -0xABCD_EF01 == ~~-0xABCD_EF01",true)]
         public void ConstantBitInvert(string expression, object expected) 
             => TestTools.AssertConstantCalc("y",expression, expected);
-        
+
+        // Todo ui16 i16, u8 does not support shift.
+        // Should they?
+        //[TestCase("y:byte = 1<<9")]
+        //[TestCase("y:byte = 255<<1")]
+        //[TestCase("y:uint16 = 0xFFFF<<1")]
+        //[TestCase("y:uint16 = 0x1<<17")]
+        //[TestCase("y:int16 = 0x1<<17")]
+
+        [TestCase("y:uint32 = 0xFFFF_ffff<<1")]
+        [TestCase("y:uint32 = 0x1<<33")]
+        [TestCase("y:uint64 = 0xFFFF_ffff_FFFF_ffff<<1")]
+        [TestCase("y:uint64 = 0x1<<65")]
+        [TestCase("y:int32 = 0x1<<33")]
+        [TestCase("y:int64 = 0x1<<65")]
+        public void Oops(string expr)
+        {
+            var rt = FunBuilder.Build(expr);
+            Assert.Throws<FunParseException>(
+                ()=> rt.Calculate());
+        }
         [TestCase("y = ^2")]
         [TestCase("y = 2^^")]
         [TestCase("y = ^^2")]
