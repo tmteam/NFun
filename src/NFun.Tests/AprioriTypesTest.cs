1ef3401eac1a514b2f1613ef6e8e3bc6cd504f07
Iurii.Sukhanov <Iurii.Sukhanov@jetbrains.com>
2021-03-21 00:24:50 +0300

    [Red] Apriory types test

--- src/NFun.Tests/AprioriTypesTest.cs
+++ src/NFun.Tests/AprioriTypesTest.cs
@@ -1,4 +1,6 @@
+using System;
 using NFun;
+using NFun.Exceptions;
 using NFun.Types;
 using NUnit.Framework;
 
@@ -7,11 +9,10 @@ namespace Funny.Tests
     public class AprioriTypesTest
     {
         [Test]
-        public void AprioriInputCalcs()
+        public void AprioriInputSpecified_CalcsWithCorrectType()
         {
-            string	expression = "y = x";
             var runtime = FunBuilder
-                .With(expression)
+                .With("y = x")
                 .WithAprioriInput("x", VarType.Text)
                 .Build();
             var res = runtime.Calculate(VarVal.New("x","test"));
@@ -21,11 +22,23 @@ namespace Funny.Tests
         }
         
         [Test]
-        public void AprioriOutputCalcs()
+        public void InputVariableSpecifiedAndDoesNotConflict_AprioriInputCalcs()
         {
-            string	expression = "y = x";
             var runtime = FunBuilder
-                .With(expression)
+                .With("x:text; y = x")
+                .WithAprioriInput("x", VarType.Text)
+                .Build();
+            var res = runtime.Calculate(VarVal.New("x","test"));
+            res.AssertReturns(VarVal.New("y","test"));
+            Assert.AreEqual(VarType.Text, runtime.Inputs[0].Type,"input");
+            Assert.AreEqual(VarType.Text, runtime.Outputs[0].Type,"output");
+        }
+        
+        [Test]
+        public void OutputVarSpecifiedAndDoesNotConflict_AprioriOutputCalcs()
+        {
+            var runtime = FunBuilder
+                .With("y:text = x")
                 .WithAprioriOutput("y", VarType.Text)
                 .Build();
             var res = runtime.Calculate(VarVal.New("x","test"));
@@ -34,5 +47,99 @@ namespace Funny.Tests
             Assert.AreEqual(VarType.Text, runtime.Outputs[0].Type,"output");
 
         }
+        
+        [Test]
+        public void InputAprioriDoesNotUsed_Calculates()
+        {
+            var runtime = FunBuilder
+                .With("y:text = x")
+                .WithAprioriInput("omega", VarType.Text)
+                .Build();
+            var res = runtime.Calculate(VarVal.New("x","test"));
+            res.AssertReturns(VarVal.New("y","test"));
+            Assert.AreEqual(VarType.Text, runtime.Inputs[0].Type,"input");
+            Assert.AreEqual(VarType.Text, runtime.Outputs[0].Type,"output");
+
+        }
+        
+        [Test]
+        public void OutputVariableDoesNotUsed_Calculates()
+        {
+            var runtime = FunBuilder
+                .With("x:text; y = x")
+                .WithAprioriOutput("alfa", VarType.Text)
+                .Build();
+            var res = runtime.Calculate(VarVal.New("x","test"));
+            res.AssertReturns(VarVal.New("y","test"));
+            Assert.AreEqual(VarType.Text, runtime.Inputs[0].Type,"input");
+            Assert.AreEqual(VarType.Text, runtime.Outputs[0].Type,"output");
+        }
+        
+        [Test]
+        public void OutputVarSpecifiedWithDifferentAprioriType_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y:int = x").WithAprioriOutput("y", VarType.Text));
+
+        [Test]
+        public void InputVarSpecifiedWithAprioriOutputConflict_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("x:int; y = x").WithAprioriOutput("y", VarType.Text));
+        
+        [Test]
+        public void OutputVarSpecifiedHasInputNameWithSameName_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y:int = x").WithAprioriOutput("x", VarType.Text));
+        
+        [Test]
+        public void OutputVarSpecifiedHasInputNameWithSameNameAndType_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y:text = x").WithAprioriOutput("x", VarType.Text));
+        
+        [Test]
+        public void OutputVarSpecifiedHasInputNameWithSameWithoutType_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y = x").WithAprioriOutput("x", VarType.Int16));
+        
+        [Test]
+        public void InputVarSpecifiedHasOutputNameWithSameName_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y:int = x").WithAprioriInput("y", VarType.Text));
+        
+        [Test]
+        public void InputVarSpecifiedHasOutputNameWithSameNameWithoutType_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y = x").WithAprioriInput("y", VarType.Bool));
+        [Test]
+        public void SpecifyTwoSameInputs_throws()
+        {
+            var builder = FunBuilder.With("x = y").WithAprioriInput("x", VarType.Text);
+            Assert.Throws<ArgumentException>(() => builder.WithAprioriInput("x", VarType.Text));
+        }
+        [Test]
+        public void SpecifyTwoSameOutputs_throws()
+        {
+            var builder = FunBuilder.With("x = y").WithAprioriOutput("y", VarType.Text);
+            Assert.Throws<ArgumentException>(() => builder.WithAprioriOutput("y", VarType.Text));
+        }
+        
+        [Test]
+        public void SpecifySameInputAndOutput_throws()
+        {
+            var builder = FunBuilder.With("z").WithAprioriInput("x", VarType.Text);
+            Assert.Throws<ArgumentException>(() => builder.WithAprioriOutput("x", VarType.Text));
+        }
+        [Test]
+        public void SpecifySameOutputAndInput_throws()
+        {
+            var builder = FunBuilder.With("z").WithAprioriOutput("y", VarType.Text);
+            Assert.Throws<ArgumentException>(() => builder.WithAprioriInput("y", VarType.Text));
+        }
+        
+        [Test]
+        public void InputVarSpecifiedHasOutputNameWithSameNameAndType_throws() =>
+            AssertFunParseError(
+                FunBuilder.With("y:text = x").WithAprioriInput("y", VarType.Text));
+
+        private void AssertFunParseError(FunBuilder builder) => Assert.Throws<FunParseException>(() => builder.Build());
     }
 }
